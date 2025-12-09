9e3ad703eb43a3e2e0e4530a55241f7892f698cf
Divyanshu Bali <bali.divyanshu319@gmail.com>
2025-12-09 13:34:51 +0530

    Add `Inter` google font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,21 @@
-import type { Metadata } from "next";
 import "./globals.css";
+
+import type { Metadata } from "next";
 import BottomNav from "./components/BottomNav/BottomNav";
+import { Inter } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Misaal",
   description: "Misaal Studio",
 };
 
+const inter = Inter();
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>
         {children}
         <BottomNav />
