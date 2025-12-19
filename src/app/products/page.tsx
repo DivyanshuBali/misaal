e5830f49159c8f56c207aeb5001a11a50fe97bc3
Divyanshu Bali <bali.divyanshu319@gmail.com>
2025-12-19 17:48:01 +0530

    Add loader animation to products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,15 +2,32 @@ import ProductTile from "@/components/ProductTile/ProductTile";
 import { supabase } from "@/lib/supabase";
 
 import styles from "./page.module.css";
+import { Suspense } from "react";
 
-export default async function ProductsPage() {
+async function ProductsListings() {
   const { data } = await supabase.from("products").select("*");
-
+  
   return (
     <main className={styles.mainContainer}>
       {data?.map((product) => {
         return <ProductTile key={product.id} {...product} />;
       })}
     </main>
+  );
+}
+
+export default async function ProductsPage() {
+  return (
+    <Suspense fallback={<ProductsListingLoader />}>
+      <ProductsListings />
+    </Suspense>
+  );
+}
+
+function ProductsListingLoader() {
+  return (
+    <section className={styles.productsListingLoader}>
+      <div>LOADING..</div>
+    </section>
   );
 }
